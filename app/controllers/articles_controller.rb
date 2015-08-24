2dfd5af921f09b638b7dba9fa17eaf6401a6449a
KIGOTHO <mkigotho2@gmail.com>
2015-08-24 16:51:08 +0300

    added links to pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,22 @@
 class ArticlesController < ApplicationController
+   
+    def index
+        @articles = Article.all
+    end
     def show 
         @article = Article.find(params[:id])
     end
     def new 
     end
-   def create
-  @article = Article.new(article_params)
+    def create
+    @article = Article.new(article_params)
  
-  @article.save
-  redirect_to @article
-end
+    @article.save
+    redirect_to @article
+     end
  
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+     private
+     def article_params
+     params.require(:article).permit(:title, :text)
+      end
 end
